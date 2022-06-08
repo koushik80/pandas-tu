0a2747145aaf016c4caa4258761796aaeac97333
koushik <koushik.dey@live.com>
2022-06-08 23:12:08 +0300

    cld6

--- clean-data.py
+++ clean-data.py
@@ -121,3 +121,70 @@ df['Date'] = pd.to_datetime(df['Date'])
 #Example
 #Remove rows with a NULL value in the "Date" column:
 df.dropna(subset=['Date'], inplace=True)
+
+#Replacing Values
+#One way to fix wrong values is to replace them with something else.
+
+#In our example,
+#it is most likely a typo, and the value should be "45" instead of "450",
+#and we could just insert "45" in row 7:
+
+#Example
+#Set "Duration" = 45 in row 7:
+
+df.loc[7, 'Duration'] = 45
+
+#For small data sets you might be able to replace the wrong data one by one, but not for big data sets.
+
+#To replace wrong data for larger data sets you can create some rules,
+#e.g. set some boundaries for legal values,
+#and replace any values that are outside of the boundaries.
+
+#Example
+#Loop through all values in the "Duration" column.
+
+#If the value is higher than 120, set it to 120:
+
+for x in df.index:
+    if df.loc[x, "Duration"] > 120:
+        df.loc[x, "Duration"] = 120
+
+
+#Removing Rows
+#Another way of handling wrong data is to remove the rows that contains wrong data.
+
+#This way you do not have to find out what to replace them with,
+#and there is a good chance you do not need them to do your analyses.
+
+#Example
+#Delete rows where "Duration" is higher than 120:
+
+for x in df.index:
+    if df.loc[x, "Duration"] > 120:
+        df.drop(x, inplace = True)
+
+
+#Discovering Duplicates
+#Duplicate rows are rows that have been registered more than one time.
+
+#By taking a look at our test data set,
+#we can assume that row 11 and 12 are duplicates.
+
+#To discover duplicates, we can use the duplicated() method.
+
+#The duplicated() method returns a Boolean values for each row:
+
+#Example
+#Returns True for every row that is a duplicate, othwerwise False:
+
+print(df.duplicated())
+
+#Removing Duplicates
+#To remove duplicates, use the drop_duplicates() method.
+
+#Example
+#Remove all duplicates:
+df.drop_duplicates(inplace=True)
+
+#***Remember: The (inplace = True) will make sure that the method does NOT return a new DataFrame,
+#but it will remove all duplicates from the original DataFrame.
